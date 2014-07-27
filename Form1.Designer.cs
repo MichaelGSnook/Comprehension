ec58c89ca4689b827dd13b7fad417e753b211e46
MichaelGSnook <snook@iastate.edu>
2014-07-27 12:23:44 -0500

    Parsed out Textbox
    Parsed text and should now be able to look up definitions from here.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,42 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.Comprehend_Button = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Comprehend = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // textBox
             // 
-            this.textBox1.AcceptsReturn = true;
-            this.textBox1.Location = new System.Drawing.Point(12, 84);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(710, 615);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.UseWaitCursor = true;
+            this.textBox.AcceptsReturn = true;
+            this.textBox.Location = new System.Drawing.Point(12, 114);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(710, 585);
+            this.textBox.TabIndex = 0;
+            this.Comprehend.SetToolTip(this.textBox, "Paste a paragraph here you don\'t understand.");
+            this.textBox.UseWaitCursor = true;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // button1
+            // Comprehend_Button
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(710, 66);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Comprehend";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.comprehend_Click);
+            this.Comprehend_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Comprehend_Button.Location = new System.Drawing.Point(12, 42);
+            this.Comprehend_Button.Name = "Comprehend_Button";
+            this.Comprehend_Button.Size = new System.Drawing.Size(710, 66);
+            this.Comprehend_Button.TabIndex = 1;
+            this.Comprehend_Button.Text = "Comprehend";
+            this.Comprehend.SetToolTip(this.Comprehend_Button, "Click to Comprehend!");
+            this.Comprehend_Button.UseVisualStyleBackColor = true;
+            this.Comprehend_Button.Click += new System.EventHandler(this.comprehend_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(734, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(734, 24);
+            this.menuStrip2.TabIndex = 3;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Comprehend
+            // 
+            this.Comprehend.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.Comprehend.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(734, 711);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Comprehend_Button);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Comprehension © - Snook Software Solutions";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,8 +124,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button Comprehend_Button;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolTip Comprehend;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace Comprehension
 {
@@ -18,6 +19,85 @@ namespace Comprehension
         }
 
         private void comprehend_Click(object sender, EventArgs e)
+        {
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+
+            String input = textBox.Text;
+            ArrayList words = new ArrayList { };
+
+            //convert String[] to ArrayList and delete spaces/punctuation.
+            String [] wordsArray = input.Split(' ');
+            foreach (string s in wordsArray)
+            {
+                String sTemp = s;
+                //if empty element, do not add it to the ArrayList
+                if (sTemp.Equals(""))
+                {
+                    continue;
+                }
+                //Removing punctuation
+                if (sTemp.Contains(' '))
+                {
+                    sTemp = sTemp.Replace(" ", String.Empty);
+                }
+                if (sTemp.Contains(","))
+                {
+                    sTemp = sTemp.Replace(",", String.Empty);
+                }
+                if (sTemp.Contains("."))
+                {
+                    sTemp = sTemp.Replace(".", String.Empty);
+                }
+                if (sTemp.Contains("?"))
+                {
+                    sTemp = sTemp.Replace("?", String.Empty);
+                }
+                if (sTemp.Contains("!"))
+                {
+                    sTemp = sTemp.Replace("!", String.Empty);
+                }
+                if (sTemp.Contains("\""))
+                {
+                    sTemp = sTemp.Replace("\"", String.Empty);
+                }
+                if (sTemp.Contains("\'"))
+                {
+                    sTemp = sTemp.Replace("\'", String.Empty);
+                }
+                if (sTemp.Contains(";"))
+                {
+                    sTemp = sTemp.Replace(";", String.Empty);
+                }
+                if (sTemp.Contains(":"))
+                {
+                    sTemp = sTemp.Replace(":", String.Empty);
+                }
+           
+
+                words.Add(sTemp);
+            }
+            System.Console.WriteLine("{0}", words[2]);
+
+            foreach (string s in words)
+            {
+                System.Console.WriteLine(s);
+            }
+
+            //ArrayList words = new ArrayList();
+            //words.Add(
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }

442f725e79be261d0d73b7bfa22a4cb9b1038951
MichaelGSnook <snook@iastate.edu>
2014-08-09 21:06:21 -0500

    Working on getting word being hovered over.

--- Form1.cs
+++ Form1.cs
@@ -110,19 +110,74 @@ namespace Comprehension
         {
             Point P = new Point(e.X, e.Y);
             Char ch = richTextBox.GetCharFromPosition(P);
-            int num = richTextBox.GetCharIndexFromPosition(P);
-            //Comprehend_Button.Text = "" + num;
+            int hoverIndex = richTextBox.GetCharIndexFromPosition(P);
+            Comprehend_Button.Text = /*hoverIndex + "";*/this.getHoverdOverWord(hoverIndex, e);
         }
 
         private void testToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Word test = new Word("double");
-            richTextBox.Text = Comprehension.Word.lookUpDefinition("test");
+            Word test = new Word("dummy");
+            richTextBox.Text = test.getDefinition();
         }
 
         private void testTypeToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            richTextBox.Text = Comprehension.Word.lookUpType("funny");
+            richTextBox.Text = Comprehension.Word.lookUpType("dummy");
         }
+
+        private String getHoverdOverWord(int hoverIndex, MouseEventArgs e)
+        {
+            if (richTextBox.TextLength > 0 && hoverIndex != 0)
+            {
+
+                //convert text in textbox to a character array.
+                char[] textboxArray = richTextBox.Text.ToCharArray(0, richTextBox.TextLength);
+
+                int beginIndex = hoverIndex;
+                int endIndex = hoverIndex;
+
+                //while beginIndex is a Letter, and is greater than 1
+                while (char.IsLetter(textboxArray[beginIndex]) && beginIndex >= 1)
+                {
+                    //if preceeding char is not a letter, exit while loop, beginIndex now contains the index of the first char in desired word.
+                    if (!char.IsLetter(textboxArray[beginIndex - 1]))
+                    {
+                        break;
+                    }
+                    //if preceeding char is a letter, decrement to preceeding char's index
+                    else
+                    {
+                        beginIndex--;
+                    }
+                }
+
+                //while endIndex is a Letter, and is less than or equal to 2nd to last char in textbox
+                while (char.IsLetter(textboxArray[beginIndex]) && endIndex <= richTextBox.TextLength - 2)
+                {
+                    //if following char is not a letter, exit while loop, endIndex now contains the index of the last char in desired word.
+                    if (!char.IsLetter(textboxArray[endIndex + 1]))
+                    {
+                        break;
+                    }
+                    //if following char is a letter, increment to following char's index
+                    else
+                    {
+                        beginIndex++;
+                    }
+                }
+
+                //return desired word being hoverd over.
+                //ex. Substring method --> Substring(beginningIndex, lenght); length = endIndex - (beginIndex - 1)
+                int length = (endIndex - (beginIndex - 1));
+
+                return richTextBox.Text.Substring(beginIndex, length);
+            }
+            else
+            {
+                return "Enter text in the textbox below";
+            }
+
+        }
+
     }
 }
